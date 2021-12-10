de77a20e2a4861b186b5adf57a7713ace2aaa933
YiScond <327281883@qq.com>
2021-12-10 17:54:25 +0800

    feat:移除sample,test两个project,移除sdk-dynamic project,sdk-core project的一些module.

--- projects/sdk/core/loader/src/main/java/com/tencent/shadow/core/loader/delegates/DI.java
+++
@@ -1,5 +0,0 @@
-package com.tencent.shadow.core.loader.delegates;
-
-public interface DI {
-    void inject(ShadowDelegate delegate, String partKey);
-}
--- projects/sdk/core/loader/src/main/java/com/tencent/shadow/core/loader/exceptions/CreateApplicationException.java
+++
@@ -1,8 +0,0 @@
-package com.tencent.shadow.core.loader.exceptions;
-
-public class CreateApplicationException extends Exception {
-
-    public CreateApplicationException(Throwable cause) {
-        super(cause);
-    }
-}
